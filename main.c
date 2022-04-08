498c080f6c72f49cc05c3322933ca503e127ee78
unknown <staub.joao.cs@gmail.com>
2022-04-07 21:20:04 -0300

    OBRIGADO DEUS POR MAIS UM DIA DE VIDA

--- main.c
+++ main.c
@@ -3,11 +3,11 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define BLOCO 8
-#define LENRAM 1024
-#define LENCACHE 16
-#define NUMACESSOMIN 20
-#define NUMACESSOMAX 30
+#define BLOCO 2
+#define LENRAM 16
+#define LENCACHE 2
+#define NUMACESSOMIN 5
+#define NUMACESSOMAX 10
 
 int contCacheImp = 1, hit = 0, miss = 0, modVet[10] = {1, 0, 0, 0, 1, 0, 0, 1, 1, 0};
 
@@ -21,17 +21,18 @@ typedef struct
   int ocupada;
   int elemento[BLOCO];
   int m[BLOCO];
-  int indiceM[BLOCO];
+  int indice[BLOCO];
 } ElemCache;
 
 void writeBack(ElemCache *cache, ElemRam *ram, int indice);
 int verificaCache(ElemCache *cache);
+void fifo(int *acessos, ElemRam *ram, ElemCache *cache, int *modifica, int qtdAcesso);
 void aleatorio(int *acessos, ElemRam *ram, ElemCache *cache, int *modifica, int qtdAcesso);
+void imprimeRam(ElemRam *ram);
 void imprimeCache(ElemCache *cache);
 int randomInt(int min, int max);
 int *gerarAcessos(int n, ElemRam *ram, int *modifica);
 void iniciaCache(ElemCache *cache);
-int cacheCheia(int n, ElemCache *cache);
 void imprimeSequencia(int *acessos, int *modifica, int n);
 
 int main()
@@ -55,13 +56,17 @@ int main()
   iniciaCache(cache);
 
   imprimeSequencia(acessos, modifica, aux);
+  imprimeRam(ram);
 
   // printf("\nRAM: ");
   // for (i = 0; i < LENRAM; i++)
   //   printf("%d ", ram[i].elemento);
   // printf("\n");
 
-  aleatorio(acessos, ram, cache, modifica, aux);
+  // testar depois : 7* 4 4* 2* 15 2 1
+  //  aleatorio(acessos, ram, cache, modifica, aux);
+  fifo(acessos, ram, cache, modifica, aux);
+  imprimeRam(ram);
 
   // printf("\n\nRAM: ");
   // for (i = 0; i < LENRAM; i++)
@@ -77,7 +82,7 @@ void writeBack(ElemCache *cache, ElemRam *ram, int indice)
   {
     if (cache[indice].m[i] == 1)
     {
-      ram[cache[indice].indiceM[i]].elemento = cache[indice].elemento[i];
+      ram[cache[indice].indice[i]].elemento = cache[indice].elemento[i];
     }
   }
 }
@@ -94,94 +99,112 @@ int verificaCache(ElemCache *cache)
   return -1;
 }
 
-int varreCache(ElemCache *cache, int elemento)
+int varreCache(ElemCache *cache, int elemento, int *b)
 {
   for (int i = 0; i < LENCACHE; i++)
   {
     for (int j = 0; j < BLOCO; j++)
     {
-      if (cache[i].elemento[j] == elemento)
+      if (cache[i].indice[j] == elemento)
+      {
+        *b = j;
         return i;
+      }
     }
   }
   return -1;
 }
 
+void imprimeRam(ElemRam *ram)
+{
+  printf("\n----- RAM -----\n");
+  for (int i = 0; i < LENRAM; i++)
+    printf("%4d ", i);
+  printf("\n");
+  for (int i = 0; i < LENRAM; i++)
+    printf("%4d ", ram[i].elemento);
+  printf("\n");
+}
+
 void imprimeCache(ElemCache *cache)
 {
-  char text[20];
-  int aux, *vet = malloc(BLOCO * sizeof(int));
   printf("----- CACHE %d -----\n", contCacheImp++);
   for (int i = 0; i < LENCACHE; i++)
   {
     if (!(cache[i].elemento[0] == -1))
     {
-      printf("linha %d\n", i);
+      printf("linha %d: ", i + 1);
       for (int j = 0; j < BLOCO; j++)
       {
-        printf("%d ", cache[i].elemento[j]);
-        sprintf(text, "%d", cache[i].elemento[j]);
-        vet[j] = strlen(text);
-      }
-      printf("\n");
-      for (int j = 0; j < BLOCO; j++)
-      {
-        if (j == 0)
-        {
-          printf("%d", cache[i].m[j]);
-        }
-        else
-        {
-          for (int k = 1; k < vet[j - 1]; k++)
-            printf(" ");
-          printf(" %d", cache[i].m[j]);
-        }
+        printf("%4d ", cache[i].indice[j]);
       }
       printf("\n");
     }
   }
 }
 
-void aleatorio(int *acessos, ElemRam *ram, ElemCache *cache, int *modifica, int qtdAcesso)
+int removePrimeiro(int *p)
 {
-  int mod, i, j, k, len, indiceCache, aux, z, x;
+  int aux = p[0], i;
+  for (i = 0; i < LENCACHE - 1; i++)
+  {
+    p[i] = p[i + 1];
+  }
+  p[i] = aux;
+  return aux;
+}
+
+void imprimePila(int *vetfifo, int n)
+{
+  printf("\nVET: ");
+  for (int i = 0; i < n; i++)
+  {
+    printf("%d ", vetfifo[i]);
+  }
+  printf("\n");
+}
+
+void fifo(int *acessos, ElemRam *ram, ElemCache *cache, int *modifica, int qtdAcesso)
+{
+  int mod, i, j, k, l, len, indiceCache, aux, z, x;
+
+  int vetfifo[LENCACHE];
+  l = 0;
   for (i = 0; i < qtdAcesso; i++)
   {
     mod = acessos[i] % BLOCO;
     len = (acessos[i] + BLOCO) - mod;
     aux = verificaCache(cache);
-    z = varreCache(cache, ram[acessos[i]].elemento);
+    z = varreCache(cache, acessos[i], &x);
+
     if (z > -1)
     {
+      if (modifica[i] = 1)
+        cache[z].elemento[x] = cache[z].elemento[x] + 1;
       hit++;
     }
     else
     {
-      if (aux > -1)
+      if (aux > -1 && l < LENCACHE)
       {
         indiceCache = aux;
+        vetfifo[l] = aux;
+        l++;
         cache[indiceCache].ocupada = 1;
       }
       else
       {
-        indiceCache = randomInt(0, LENCACHE - 1);
-
+        indiceCache = removePrimeiro(vetfifo);
         writeBack(cache, ram, indiceCache);
       }
       for (j = (acessos[i] - mod), k = 0; j < len; j++, k++)
       {
-        if (modifica[i] == 1)
+        // verifica se o elemento vai ser modificado
+        cache[indiceCache].indice[k] = j;
+        if (modifica[i] == 1 && acessos[i] == j)
         {
-          if (acessos[i] == j)
-          {
-            cache[indiceCache].elemento[k] = ram[j].elemento + 1;
-            cache[indiceCache].m[k] = 1;
-            cache[indiceCache].indiceM[k] = j;
-          }
-          else
-          {
-            cache[indiceCache].elemento[k] = ram[j].elemento;
-          }
+          cache[indiceCache].elemento[k] = ram[j].elemento + 1;
+          cache[indiceCache].m[k] = 1;
         }
         else
         {
@@ -194,6 +217,67 @@ void aleatorio(int *acessos, ElemRam *ram, ElemCache *cache, int *modifica, int 
   }
 }
 
+// acesso aleatorio
+void aleatorio(int *acessos, ElemRam *ram, ElemCache *cache, int *modifica, int qtdAcesso)
+{
+  int mod, i, j, k, len, indiceCache, aux, z, x;
+
+  // for para todos acessos gerados
+  for (i = 0; i < qtdAcesso; i++)
+  {
+    // resto da divisão do acesso pelo tamanho do bloco
+    mod = acessos[i] % BLOCO;
+    // variavel auxiliar para carregar os blocos na cache
+    len = (acessos[i] + BLOCO) - mod;
+    // retorna o primeiro incice não ocupado ou -1 caso a cache estiver cheia
+    aux = verificaCache(cache);
+    // verifica se o elemento já está na cache
+    z = varreCache(cache, acessos[i], &x);
+
+    // se estiver na cache conta um acerto
+    if (z > -1)
+    {
+      if (modifica[i] = 1)
+        cache[z].elemento[x] = cache[z].elemento[x] + 1;
+      hit++;
+    }
+    // caso contrario armazena na cache ou realiza a subistituição
+    else
+    {
+      // ainda tem elementos sobrando
+      if (aux > -1)
+      {
+        // adiciona sequencialmente
+        indiceCache = aux;
+        cache[indiceCache].ocupada = 1;
+      }
+      else
+      {
+        // gera um valor aleatorio para adicionar na cache
+        indiceCache = randomInt(0, LENCACHE - 1);
+        // e escreve a modificação na RAM
+        writeBack(cache, ram, indiceCache);
+      }
+      for (j = (acessos[i] - mod), k = 0; j < len; j++, k++)
+      {
+        // verifica se o elemento vai ser modificado
+        cache[indiceCache].indice[k] = j;
+        if (modifica[i] == 1 && acessos[i] == j)
+        {
+          cache[indiceCache].elemento[k] = ram[j].elemento + 1;
+          cache[indiceCache].m[k] = 1;
+        }
+        else
+        {
+          cache[indiceCache].elemento[k] = ram[j].elemento;
+        }
+      }
+      miss++;
+    }
+    imprimeCache(cache);
+  }
+}
+// bloco de código para gerar acessos na memoria RAM
 int randomInt(int min, int max) { return min + rand() % (max + 1 - min); }
 
 int *gerarAcessos(int n, ElemRam *ram, int *modifica)
@@ -209,17 +293,7 @@ int *gerarAcessos(int n, ElemRam *ram, int *modifica)
   return vet;
 }
 
-int cacheCheia(int n, ElemCache *cache)
-{
-  int cont = 0;
-  for (int i = 0; i < n; i++)
-  {
-    if (cache[i].ocupada == 0)
-      cont++;
-  }
-  return cont;
-}
-
+// inicia memoria cache
 void iniciaCache(ElemCache *cache)
 {
   for (int i = 0; i < LENCACHE; i++)
@@ -229,7 +303,7 @@ void iniciaCache(ElemCache *cache)
     {
       cache[i].elemento[j] = -1;
       cache[i].m[j] = 0;
-      cache[i].indiceM[j] = -1;
+      cache[i].indice[j] = -1;
     }
   }
 }
